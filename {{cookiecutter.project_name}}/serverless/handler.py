9910263de5954f55eb367a33e7a5107523dc3172
Brian Zambrano <brianz@gmail.com>
2018-05-18 22:54:54 -0600

    Updates to give a better out-of-the-box experience
    Why
    ---
    
    - What we had was working but was overly simplistic
    
    This change addresses the need by
    ---------------------------------
    
    - Adding serverless directory with workable and sane defaults
    - What we had was working but was overly simplistic\l
    - Modernizing the Makefile
    - Spliting out python and serverless versions from testing
    - Adding AWS_REGION as a choice

---
+++ {{cookiecutter.project_name}}/serverless/handler.py
@@ -0,0 +1,16 @@
+"""Template for a Python3 project on AWS."""
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
