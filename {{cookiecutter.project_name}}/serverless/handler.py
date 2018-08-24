51985bf3d7de1807498f90748312f53fae16edea
Brian Zambrano <brianz@gmail.com>
2018-08-24 17:12:14 -0600

    Improvments plus add v 1.28.0
    Why
    ---
    
    - General updates and improvements
    
    This change addresses the need by
    ---------------------------------
    
    - Add Serverless 1.28.0 as a choice
    - Drop 1.25
    - Remove option to use Python 2
    - Add yapf and flake8 formatting config
    - Add .gitignore for `envs` directory

--- {{cookiecutter.project_name}}/serverless/handler.py
+++ {{cookiecutter.project_name}}/serverless/handler.py
@@ -1,16 +1,25 @@
 """Template for a Python3 project on AWS."""
 import json
+import sys
+
+from pathlib import Path
+
+# pylint: disable=wrong-import-position
+
+# Munge our sys path so libs can be found
+LIB = Path(__file__).resolve().parent / 'lib'
+sys.path.insert(0, str(LIB))
 
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": event,
     }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
     }
 
     return response
